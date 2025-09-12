14da3e9ac4161c2e0fdf749e98c73d8e7140552d
Amin Bihamta <97396978+AminBihamta@users.noreply.github.com>
2025-09-12 23:16:54 +0200

    Optimized room list layout

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -20,19 +20,20 @@ export default async function AdminPage() {
     const supabase = createClient(cookies());
     const { data: rooms } = await supabase.from("rooms").select();
     return (
-        <div>
+        <div className='flex flex-col min-w-screen'>
             <AddRoomForm />
-            <div className='flex flex-col gap-4'>
-                <div>
-                    <p>Room Id</p>
-                    <p>Room Name</p>
+            <div className='flex flex-col gap-4 min-w-full'>
+                <div className='flex flex-row min-w-full justify-between'>
+                    <p className='w-[30%] text-center'>Room Id</p>
+                    <p className='w-[30%] text-center'>Room Name</p>
+                    <p className='w-[300px] text-center'>Actions Name</p>
                 </div>
 
                 {rooms?.map((room) => (
-                    <div key={room.id}>
-                        <p>{room.id}</p>
-                        <p>{room.name}</p>
-                        <DeleteRoomButton roomId={room.id} />
+                    <div className='flex flex-row min-w-full justify-between' key={room.id}>
+                        <p className='w-[30%] text-center'>{room.id}</p>
+                        <p className='w-[30%] text-center'>{room.name}</p>
+                        <div className='w-[300px] text-center'><DeleteRoomButton roomId={room.id} /></div>
                     </div>
                 ))}
             </div>
